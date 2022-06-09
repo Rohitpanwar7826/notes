38d498c3fe4e51ed360da09bac8f013d92ac7840
Rohit panwar <rohitpawar5894@gmail.com>
2022-06-09 22:55:46 +0530

    Update Header.js

--- src/componets/Header.js
+++ src/componets/Header.js
@@ -10,12 +10,12 @@ export default function Header() {
                 </div>
                 <div className="w-auto p-2">
                     <div className="text-sm flex items-center justify-center">
-                        <NavLink to="/" className="items inline-block  mt-0 text-teal-lighter hover:text-white mr-4 p-1 rounded-xl" style={({isActive})=>{
+                        <NavLink to="/notes" className="items inline-block  mt-0 text-teal-lighter hover:text-white mr-4 p-1 rounded-xl" style={({isActive})=>{
                             return {backgroundColor: isActive ? 'white' : ''}
                         }}>
                             Notes
                         </NavLink>
-                        <NavLink to="about" className="items inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4 p-1 rounded-xl" style={({isActive})=>{
+                        <NavLink to="notes/about" className="items inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4 p-1 rounded-xl" style={({isActive})=>{
                             return {backgroundColor: isActive ? 'white' : ''}
                         }}>
                             About
