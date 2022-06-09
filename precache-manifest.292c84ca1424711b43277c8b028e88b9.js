4989a9b355486c07f605a696d1ee4faf19206db7
Rohit panwar <rohitpawar5894@gmail.com>
2022-06-09 22:37:35 +0530

    Updates

---
+++ precache-manifest.292c84ca1424711b43277c8b028e88b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "79ab85c11efedc717a68",
+    "url": "/notes/static/css/main.6bd579be.chunk.css"
+  },
+  {
+    "revision": "79ab85c11efedc717a68",
+    "url": "/notes/static/js/main.79ab85c1.chunk.js"
+  },
+  {
+    "revision": "8c98aab82769540efb62",
+    "url": "/notes/static/js/1.8c98aab8.chunk.js"
+  },
+  {
+    "revision": "49e72d48d085af594b5f",
+    "url": "/notes/static/js/2.49e72d48.chunk.js"
+  },
+  {
+    "revision": "282b43dd54f67d640fd8",
+    "url": "/notes/static/js/runtime~main.282b43dd.js"
+  },
+  {
+    "revision": "ee4d166b3da44936c3b32716e745ae9c",
+    "url": "/notes/static/media/rohit.ee4d166b.jpeg"
+  },
+  {
+    "revision": "b5e7e0acaa0b23d321c87b7ea026c64b",
+    "url": "/notes/index.html"
+  }
+];
